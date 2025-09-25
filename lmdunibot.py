99d95788dd3f5d64affbe804d63e08ff235e9759
adamotaku541-eng <adamotaku541@gmail.com>
2025-09-25 14:18:38 +0100

    Add files via upload

--- lmdunibot.py
+++ lmdunibot.py
@@ -86,6 +86,10 @@ async def remove_webhook(application):
 def main():
     print("✅ البوت قيد التشغيل...")
 
+    # التحقق من وجود التوكن
+    if not TOKEN:
+        raise ValueError("❌ لم يتم العثور على التوكن في متغير البيئة BOT_TOKEN")
+
     threading.Thread(target=run_flask).start()
 
     application = Application.builder().token(TOKEN).build()
